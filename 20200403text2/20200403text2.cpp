8af202ca2fc2c12879bdb7f5ade7c1a2270e18dd
yezerli <1123603008@qq.com>
2020-04-03 14:39:27 +0800

    234完成

--- 20200403text2/20200403text2.cpp
+++ 20200403text2/20200403text2.cpp
@@ -8,6 +8,7 @@ using namespace std;
 int main(){
 	cv::Mat dstMat2;
 	cv::Mat dstMat3;
+	cv::Mat dstMat4;
 	cv::Mat srcMat = cv::imread("D:\\lena.jpg", 1);
 	if (srcMat.empty()) return -1;
 	//旋转和缩放操作
@@ -31,12 +32,28 @@ int main(){
 								cv::Point2f(200,100) };
 	//计算仿射矩阵
 	const cv::Mat affine_matrix2 = cv::getAffineTransform(src_pt, dst_pt);
-
 	cv::warpAffine(srcMat, dstMat3, affine_matrix2, srcMat.size());
+	//投影变换操作
+	//变换前四点坐标
+	const cv::Point2f pts1[] = {
+									cv::Point2f(150,150),
+									cv::Point2f(150,300),
+									cv::Point2f(350,300),
+									cv::Point2f(350,150) };
+	//变换后四点坐标
+	const cv::Point2f pts2[] = {
+								cv::Point2f(200,150),
+								cv::Point2f(200,300),
+								cv::Point2f(340,270),
+								cv::Point2f(340,180) };
+	//透视变换行列计算
+	cv::Mat perspective_matrix = cv::getPerspectiveTransform(pts1, pts2);
+	cv::warpPerspective(srcMat, dstMat4, perspective_matrix, srcMat.size());
 
 	cv::imshow("src", srcMat);
 	cv::imshow("dst2", dstMat2);
 	cv::imshow("dst3", dstMat3);
+	cv::imshow("dst4", dstMat4);
 	cv::waitKey(0);
 
 
